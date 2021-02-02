48995d43b33eb822c8a04e281184aebbe203e148
Matias Pando <matiaspando@gmail.com>
2021-02-01 21:42:15 -0300

    Prueba 1

---
+++ js/ui.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'right'});
+  // add recipe form
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, {edge: 'left'});
+});
+
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+  console.log('Smile!')
+	context.drawImage(video, 0, 0, 640, 480);
+});
