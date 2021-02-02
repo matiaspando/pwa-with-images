5636900c4388284b93f958becd1345213e3594f2
Matias Pando <matiaspando@gmail.com>
2021-02-01 22:18:38 -0300

    Prueba 3

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
+  M.Sidenav.init(menus, { edge: 'right' });
   // add recipe form
   const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+  M.Sidenav.init(forms, { edge: 'left' });
 });
 
 // Elements for taking the snapshot
@@ -15,21 +15,28 @@ var video = document.getElementById('video');
 
 
 // Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  // Not adding `{ audio: true }` since we only want video now
+  navigator.mediaDevices.getUserMedia
+    ({
+      video: {
+        facingMode: {
+          exact: 'environment'
+        }
+      }
+    }).then(function (stream) {
+      //video.src = window.URL.createObjectURL(stream);
+      video.srcObject = stream;
+      video.play();
     });
 } else {
   console.log('Hay un error')
-  console.log('navigator.mediaDevices',navigator.mediaDevices)
-  console.log('navigator.mediaDevices.getUserMedia',navigator.mediaDevices.getUserMedia)
+  console.log('navigator.mediaDevices', navigator.mediaDevices)
+  console.log('navigator.mediaDevices.getUserMedia', navigator.mediaDevices.getUserMedia)
 }
 
 // Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
+document.getElementById("snap").addEventListener("click", function () {
   console.log('Smile!')
-	context.drawImage(video, 0, 0, 160, 120);
+  context.drawImage(video, 0, 0, 160, 120);
 });
